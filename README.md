4727a9270c86d47089cd3608ab55f5d8069ed5ae
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-11-03 11:26:29 +0530

    First drop in the ocean
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

---
+++ README.md
@@ -0,0 +1,15 @@
+Tokinar
+=========
+
+A simple Webinar application based on OpenTok.
+
+## Requirements
+
+- NodeJS v4+
+- TokBox Account (API Key and Secret)
+
+## Install
+
+- Copy `config.sample.js` to `config.js` and edit configuration.
+- Install dependencies: `npm install`
+- Start the application with: `npm start`
---
+++ config.sample.js
@@ -0,0 +1,47 @@
+// Copyright 2016 Kaustav Das Modak
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License. You may
+// obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+
+module.exports = {
+
+  /**
+   * Application settings
+   */
+  app: {
+
+    /**
+     * Port to host the application on. Default: 8080.
+     */
+    port: 8080
+  },
+
+  // ----------------------------------------------
+
+  /**
+   * TokBox OpenTok settings
+   */
+  opentok: {
+
+    /**
+     * API Key obtained from TokBox
+     */
+    api_key: "xxxx",
+
+    /**
+     * API Secret obtained from TokBox
+     */
+    api_secret: "xxxx"
+  }
+
+};
