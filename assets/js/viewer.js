8df018a9acb55c415bcca0744aa801869d7fed1a
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-11-11 12:08:17 +0530

    First cut of viewer picture-in-picture
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -8,14 +8,23 @@
   "use strict";
 
   var _attrs = Tokinar.get_opentok_attrs(),
+      _feeds = { screen: false, camera: false },
       _session;
 
   var handle_stream_created = function (evt) {
-    _session.subscribe(evt.stream, $("#presenter"), {
-      insertMode: "append",
-      width: 640,
-      height: 480
-    });
+    if (evt.stream.videoType === "screen") {
+      _session.subscribe(evt.stream, $("#presenter-screen"), {
+        insertMode: "append",
+        width: 640,
+        height: 480
+      });
+    } else {
+      _session.subscribe(evt.stream, $("#presenter-camera"), {
+        insertMode: "append",
+        width: 120,
+        height: 90
+      });
+    }
   };
 
   var setup_handlers = function (session) {
